5166625903ba2af5fed1669c7257e549cd61cbba
lienzhu <56902061+lienzhu@users.noreply.github.com>
2020-04-01 17:00:10 +1100

    Initial commit

---
+++ app/src/main/java/au/edu/unsw/infs3634/cryptobag/MainActivity.java
@@ -0,0 +1,40 @@
+package au.edu.unsw.infs3634.cryptobag;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+
+import java.util.List;
+
+import au.edu.unsw.infs3634.cryptobag.entities.Coin;
+import au.edu.unsw.infs3634.cryptobag.entities.CoinLoreResponse;
+
+public class MainActivity extends AppCompatActivity {
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.detail_container) != null) {
+            mTwoPane = true;
+        }
+
+        RecyclerView mRecyclerView = findViewById(R.id.rvList);
+        mRecyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        Gson gson = new Gson();
+        CoinLoreResponse response = gson.fromJson(CoinLoreResponse.json, CoinLoreResponse.class);
+        List<Coin> coins = response.getData();
+        RecyclerView.Adapter mAdapter = new CoinAdapter(this, coins, mTwoPane);
+
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}
---
+++ app/src/main/java/au/edu/unsw/infs3634/cryptobag/entities/Info.java
@@ -0,0 +1,68 @@
+package au.edu.unsw.infs3634.cryptobag.entities;
+
+import java.io.Serializable;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+//import org.apache.commons.lang.builder.ToStringBuilder;
+
+public class Info implements Serializable
+{
+
+    @SerializedName("coins_num")
+    @Expose
+    private Integer coinsNum;
+    @SerializedName("time")
+    @Expose
+    private Integer time;
+    private final static long serialVersionUID = 6132040084086371856L;
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public Info() {
+    }
+
+    /**
+     *
+     * @param time
+     * @param coinsNum
+     */
+    public Info(Integer coinsNum, Integer time) {
+        super();
+        this.coinsNum = coinsNum;
+        this.time = time;
+    }
+
+    public Integer getCoinsNum() {
+        return coinsNum;
+    }
+
+    public void setCoinsNum(Integer coinsNum) {
+        this.coinsNum = coinsNum;
+    }
+
+    public Info withCoinsNum(Integer coinsNum) {
+        this.coinsNum = coinsNum;
+        return this;
+    }
+
+    public Integer getTime() {
+        return time;
+    }
+
+    public void setTime(Integer time) {
+        this.time = time;
+    }
+
+    public Info withTime(Integer time) {
+        this.time = time;
+        return this;
+    }
+
+//    @Override
+//    public String toString() {
+//        return new ToStringBuilder(this).append("coinsNum", coinsNum).append("time", time).toString();
+//    }
+
+}
